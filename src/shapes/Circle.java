6bf4953c2ca62419681bcaccf2070b92c053c74b
rphoke <rphoke@csee137aer41.lcsee.wvu.edu>
2017-10-11 19:15:55 -0400

    This is the circle yesh yesh

--- src/shapes/Circle.java
+++ src/shapes/Circle.java
@@ -8,12 +8,19 @@ public class Circle extends Shape {
 	 * below.  Circles must have a positive radius.
 	 */
 	public Circle(double radius) throws IllegalArgumentException {
-		// TODO Implement me
+		if (radius > 0)
+			this.radius = radius; 
+		else
+			throw new IllegalArgumentException("Radius cannot be negative or zero, fool.");
 	}
 	
 	/*
 	 *  Implement any remaining methods demanded by the superclass.
 	 */
+	public double calculateArea()
+	{
+		return Math.PI*radius*radius;
+	}
 	
 	public double getRadius() {
 		return radius;
